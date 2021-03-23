ac4e30e2123daa6fe7ae1d6fbe5fd233d95d8c33
ga <gazhanghb@163.com>
2021-03-23 09:13:01 +0800

    sdf

--- 06_ES6_Babel_Browserify/js/src/module1.js
+++ 06_ES6_Babel_Browserify/js/src/module1.js
@@ -2,15 +2,16 @@
 * module1使用了【分别暴露】的方式
 * */
 
-export let data = 'atguigu'
+export let data = "Module Es6";
 
-export function demo1() {
-  console.log(`我是module1里的demo函数`,data.toUpperCase())
+export function demo() {
+  console.log("我是module1里边的demo函数",data.toUpperCase());
 }
 
-export function test1() {
-  console.log('我是module1里的test函数',data.toLowerCase())
+export function test() {
+  console.log("我是module1里边的test函数",data.toLowerCase());
 }
+
 
 //以下代码与暴露无关，是module1内部自己在用的东西
 /*
