bb8cbc371bac7819304d3c69896806c4751df9e1
ga <gazhanghb@163.com>
2021-03-23 10:06:15 +0800

    sdf

--- 06_ES6_Babel_Browserify/js/dist/module1.js
+++ 06_ES6_Babel_Browserify/js/dist/module1.js
@@ -1,21 +1,22 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.demo1 = demo1;
-exports.test1 = test1;
+exports.demo = demo;
+exports.test = test;
 /*
 * module1使用了【分别暴露】的方式
 * */
-var data = exports.data = 'atguigu';
 
-function demo1() {
-  console.log('我是module1里的demo函数', data.toUpperCase());
+var data = exports.data = "Module Es6";
+
+function demo() {
+  console.log("我是module1里边的demo函数", data.toUpperCase());
 }
 
-function test1() {
-  console.log('我是module1里的test函数', data.toLowerCase());
+function test() {
+  console.log("我是module1里边的test函数", data.toLowerCase());
 }
 
 //以下代码与暴露无关，是module1内部自己在用的东西
